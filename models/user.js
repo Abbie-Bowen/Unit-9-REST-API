03460cbfcdbbd5b1e0f4d1d394cc25100b47fb45
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-10-05 15:29:48 -0700

    models completed

---
+++ models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model, DataTypes, INTEGER } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class User extends Model {}
+    User.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        emailAddress: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, { sequelize });
+
+    User.associate = (models) => {
+        User.hasMany(models.Course, {
+            foreignKey: {
+                fieldName: "userId",
+                allowNull: false,
+            },
+        });
+    };
+
+    return User;
+}
